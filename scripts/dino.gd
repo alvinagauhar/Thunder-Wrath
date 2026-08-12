7f73d8508026af5c7b9afc8e51a2261760664077
alvinagauhar <alvinagauhar@gmail.com>
2026-08-13 00:49:06 +0500

    add gravity and jump movement

--- scripts/dino.gd
+++ scripts/dino.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
-@export var SPEED= 400
+@export var movespeed= 400
+@export var gravity= 980.0
+@export var jumpspeed= -400
+
 func _physics_process(delta: float) -> void:
+	#Gravity 
+	if not is_on_floor():
+		velocity.y= velocity.y + gravity * delta
+	else:
+		velocity.y=0
+	#jumping
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = jumpspeed
 	
 	#sideway movement
 	if Input.is_action_pressed("left"):
-		velocity= Vector2(-200,0)
+		velocity.x=-movespeed
 	elif Input.is_action_pressed("right"):
-		velocity= Vector2(200,0)
+		velocity.x= movespeed
 	else:
-		velocity=Vector2(0,0)
+		velocity.x=0
 	move_and_slide()
